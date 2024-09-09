d4e2643174dc606e79960e8bdb11ee354504487f
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-09-09 14:45:16 -0400

    feat: add projects

---
+++ AdSetIntegrador/src/AdSetIntegrador.Api/Controllers/HealthChecker/HealthCheckerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdSetIntegrador.Api.Controllers.HealthChecker;
+
+[Route("api/health-checker")]
+[ApiController]
+public class HealthCheckerController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Handle()
+    {
+        return Ok("Service running.");
+    }
+}
