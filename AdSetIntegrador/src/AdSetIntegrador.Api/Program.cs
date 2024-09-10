c5852f222c4895c232850bffaa06c85a133fcc4d
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-09-09 21:54:38 -0400

    feat: add exception filter

--- AdSetIntegrador/src/AdSetIntegrador.Api/Program.cs
+++ AdSetIntegrador/src/AdSetIntegrador.Api/Program.cs
@@ -1,3 +1,4 @@
+using AdSetIntegrador.Api.Filters;
 using AdSetIntegrador.Application;
 using AdSetIntegrador.Infrastructure;
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
