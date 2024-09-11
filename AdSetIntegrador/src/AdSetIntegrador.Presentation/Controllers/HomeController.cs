b6d84a70dbb87465c0ec90a9055f15982a24c5dc
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-09-10 23:24:03 -0400

    feat: add presentation project + vehicles home overview

---
+++ AdSetIntegrador/src/AdSetIntegrador.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using AdSetIntegrador.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdSetIntegrador.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AdSetIntegrador/src/AdSetIntegrador.Presentation/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdSetIntegrador.Presentation.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
