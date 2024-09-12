fd5067894828ec025bd08aa58d99bf9101ff5cf7
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-09-11 22:54:53 -0400

    feat: add vehicle register page

--- AdSetIntegrador/src/AdSetIntegrador.Presentation/Controllers/VehicleController.cs
+++ AdSetIntegrador/src/AdSetIntegrador.Presentation/Controllers/VehicleController.cs
@@ -1,6 +1,10 @@
 ﻿using AdSetIntegrador.Application.UseCases.Vehicles.List;
+using AdSetIntegrador.Application.UseCases.Vehicles.Register;
+using AdSetIntegrador.Communication.Requests;
 using AdSetIntegrador.Presentation.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace AdSetIntegrador.Presentation.Controllers
 {
@@ -24,6 +28,39 @@ namespace AdSetIntegrador.Presentation.Controllers
             });
 
             return View(vehicleModel);
+        }
+
+        public IActionResult Register()
+        {
+            ViewBag.Action = "Register";
+            return View("Register", new VehicleModel());
+        }
+
+        [HttpPost]
+        public IActionResult Register(
+            VehicleModel vehicle,
+            [FromServices] IRegisterVehicleUseCase useCase
+        ) {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    useCase.Execute(new RequestRegisterVehicleJson
+                    {
+                        Brand = vehicle.Brand,
+                        Color = vehicle.Color,
+                        Mileage = vehicle.Mileage,
+                        Model = vehicle.Model,
+                        Plate = vehicle.Plate,
+                        Price = vehicle.Price,
+                        Year = vehicle.Year
+                    });
+                }
+                return RedirectToAction("Index");
+            }
+            catch {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
